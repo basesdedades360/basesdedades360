3cc22874fbcb0cecfade836096010f7b283a80da
basesdedades360 <jordi.quesada@proton.me>
2026-05-30 07:04:20 +0200

    Update 02_institut_inserts_mysql.sql

--- capitol-06/MySQL/02_institut_inserts_mysql.sql
+++ capitol-06/MySQL/02_institut_inserts_mysql.sql
@@ -34,77 +34,68 @@ INSERT INTO professor (id_prof, nom, cognoms, email, telefon, salari, data_alta,
     ( 1, 'Héctor', 'Maeso', 'hmaeso@institut.cat', '600111222', 2950.00, '2010-09-01', TRUE,  1),
     ( 2, 'Marta', 'Soler', 'msoler@institut.cat', '600222333', 2700.50, '2015-09-01', TRUE,  1),
     ( 3, 'Jordi', 'Vila', 'jvila@institut.cat', '600333444', 2450.00, '2018-09-01', TRUE,  1),
-    ( 4, 'Eva', 'Romero', 'eromero@institut.cat',  '600444555', 2600.00, '2012-09-01', FALSE, 1),
-    ( 5, 'Dori', 'Garcia', 'dgarcia@institut.cat',  '600555666', 2800.00, '2011-09-01', TRUE,  2),
-    ( 6, 'Anna', 'Martí', 'amarti@institut.cat',   '600666777', 2300.00, '2020-09-01', TRUE,  2),
+    ( 4, 'Eva', 'Romero', 'eromero@institut.cat', '600444555', 2600.00, '2012-09-01', FALSE, 1),
+    ( 5, 'Dori', 'Garcia', 'dgarcia@institut.cat', '600555666', 2800.00, '2011-09-01', TRUE,  2),
+    ( 6, 'Anna', 'Martí', 'amarti@institut.cat', '600666777', 2300.00, '2020-09-01', TRUE,  2),
     ( 7, 'Ramón', 'Redondo', 'rredondo@institut.cat', '600777888', 2550.00, '2014-09-01', TRUE,  3),
-    ( 8, 'Sergi', 'Mas', 'smas@institut.cat',     '600888999', 2200.00, '2021-09-01', TRUE,  3),
-    ( 9, 'Isabel', 'Garcia', 'igarcia@institut.cat',  '600999000', 2750.00, '2013-09-01', TRUE,  4),
-    (10, 'Pere', 'Roca', 'proca@institut.cat',    '601111222', 2900.00, '2009-09-01', TRUE,  5),
-    (11, 'Laia', 'Puig', 'lpuig@institut.cat',    '601222333', 2400.00, '2019-09-01', FALSE, 5),
-    (12, 'Núria', 'Camps', 'ncamps@institut.cat',   '601333444', 2650.00, '2016-09-01', TRUE,  6),
-    (13, 'Carles', 'Bosch', 'cbosch@institut.cat',   '601444555', 2850.00, '2012-09-01', TRUE,  7);
+    ( 8, 'Sergi', 'Mas', 'smas@institut.cat', '600888999', 2200.00, '2021-09-01', TRUE,  3),
+    ( 9, 'Isabel', 'Garcia', 'igarcia@institut.cat', '600999000', 2750.00, '2013-09-01', TRUE,  4),
+    (10, 'Pere', 'Roca', 'proca@institut.cat', '601111222', 2900.00, '2009-09-01', TRUE,  5),
+    (11, 'Laia', 'Puig', 'lpuig@institut.cat', '601222333', 2400.00, '2019-09-01', FALSE, 5),
+    (12, 'Núria', 'Camps', 'ncamps@institut.cat', '601333444', 2650.00, '2016-09-01', TRUE,  6),
+    (13, 'Carles', 'Bosch', 'cbosch@institut.cat', '601444555', 2850.00, '2012-09-01', TRUE,  7);
 
 -- ------------------------------------------------------------
 --  ALUMNES
 -- ------------------------------------------------------------
 
 INSERT INTO alumne (id_alumne, nom, cognoms, email, data_naix, telefon, curs_inici) VALUES
-    ( 1, 'Marc', 'Puig Vidal',  'marcpv@alumnes.cat',    '2005-03-12', '610111111', 2024),
-    ( 2, 'Laia', 'Solé Mas',    'laiasm@alumnes.cat',    '2004-07-22', '610222222', 2023),
-    ( 3, 'Pol', 'Riera Bosch', 'polrb@alumnes.cat',     '2005-11-05', '610333333', 2024),
-    ( 4, 'Júlia', 'Font Casas',  'juliafc@alumnes.cat',   '2003-02-18', '610444444', 2023),
-    ( 5, 'Aleix', 'Roca Vila',   'aleixrv@alumnes.cat',   '2005-09-30', NULL,        2024),
-    ( 6, 'Berta', 'Camps Tort',  'bertact@alumnes.cat',   '2004-05-14', '610666666', 2023),
-    ( 7, 'Nil', 'Bosch Soler', 'nilbs@alumnes.cat',     '2006-01-08', '610777777', 2025),
-    ( 8, 'Clara', 'Vidal Puig',  'claravp@alumnes.cat',   '2005-04-25', '610888888', 2024),
-    ( 9, 'Arnau', 'Mas Riera',   'arnaumr@alumnes.cat',   '2004-10-11', '610999999', 2023),
-    (10, 'Maria',   'Tort Font',   'mariatf@alumnes.cat',   '2005-06-19', '611000000', 2024),
-    (11, 'Èlia',    'Soler Camps', 'eliasc@alumnes.cat',    '2006-03-03', '611111111', 2025),
-    (12, 'Roger',   'Garcia Roca', 'rogergr@alumnes.cat',   '2003-12-27', '611222222', 2022),
-    (13, 'Ona',     'Casas Bosch', 'onacb@alumnes.cat',     '2005-08-15', NULL,        2024),
-    (14, 'Biel',    'Vila Mas',    'bielvm@alumnes.cat',    '2004-02-09', '611444444', 2023),
-    (15, 'Jana',    'Puig Tort',   'janapt@alumnes.cat',    '2006-05-21', '611555555', 2025),
-    (16, 'Aitor',   'Riera Camps', 'aitorrc@alumnes.cat',   '2003-09-06', '611666666', 2022),
-    (17, 'Lluc',    'Mas Vidal',   'llucmv@alumnes.cat',    '2005-12-14', '611777777', 2024),
-    (18, 'Gal·la',  'Bosch Riera', 'gallabr@alumnes.cat',   '2004-04-02', '611888888', 2023),
-    (19, 'Quim',    'Font Soler',  'quimfs@alumnes.cat',    '2005-10-28', NULL,        2024),
+    ( 1, 'Marc', 'Puig Vidal', 'marcpv@alumnes.cat', '2005-03-12', '610111111', 2024),
+    ( 2, 'Laia', 'Solé Mas', 'laiasm@alumnes.cat', '2004-07-22', '610222222', 2023),
+    ( 3, 'Pol', 'Riera Bosch', 'polrb@alumnes.cat', '2005-11-05', '610333333', 2024),
+    ( 4, 'Júlia', 'Font Casas', 'juliafc@alumnes.cat', '2003-02-18', '610444444', 2023),
+    ( 5, 'Aleix', 'Roca Vila', 'aleixrv@alumnes.cat', '2005-09-30', NULL, 2024),
+    ( 6, 'Berta', 'Camps Tort', 'bertact@alumnes.cat', '2004-05-14', '610666666', 2023),
+    ( 7, 'Nil', 'Bosch Soler', 'nilbs@alumnes.cat', '2006-01-08', '610777777', 2025),
+    ( 8, 'Clara', 'Vidal Puig', 'claravp@alumnes.cat', '2005-04-25', '610888888', 2024),
+    ( 9, 'Arnau', 'Mas Riera', 'arnaumr@alumnes.cat', '2004-10-11', '610999999', 2023),
+    (10, 'Maria', 'Tort Font', 'mariatf@alumnes.cat', '2005-06-19', '611000000', 2024),
+    (11, 'Èlia', 'Soler Camps', 'eliasc@alumnes.cat', '2006-03-03', '611111111', 2025),
+    (12, 'Roger', 'Garcia Roca', 'rogergr@alumnes.cat', '2003-12-27', '611222222', 2022),
+    (13, 'Ona', 'Casas Bosch', 'onacb@alumnes.cat', '2005-08-15', NULL, 2024),
+    (14, 'Biel', 'Vila Mas', 'bielvm@alumnes.cat', '2004-02-09', '611444444', 2023),
+    (15, 'Jana', 'Puig Tort', 'janapt@alumnes.cat', '2006-05-21', '611555555', 2025),
+    (16, 'Aitor', 'Riera Camps', 'aitorrc@alumnes.cat', '2003-09-06', '611666666', 2022),
+    (17, 'Lluc', 'Mas Vidal', 'llucmv@alumnes.cat', '2005-12-14', '611777777', 2024),
+    (18, 'Gal·la', 'Bosch Riera', 'gallabr@alumnes.cat', '2004-04-02', '611888888', 2023),
+    (19, 'Quim', 'Font Soler', 'quimfs@alumnes.cat', '2005-10-28', NULL, 2024),
     (20, 'Adriana', 'Roca Garcia', 'adrianarg@alumnes.cat', '2006-07-17', '612000000', 2025);
 
 -- ------------------------------------------------------------
 --  MÒDULS
 -- ------------------------------------------------------------
---  Codis CHAR(6). Hores entre 30 i 300. Curs 1 o 2.
---  El mòdul 'ADM201 - Gestió logística' es deixa SENSE professor (NULL)
---  per practicar LEFT JOIN i la diferència entre INNER i OUTER JOINs.
+
 INSERT INTO modul (id_modul, codi, nom_modul, hores, curs, id_prof) VALUES
-    -- Informàtica
-    ( 1, 'INF101', 'Bases de dades',                  165, 1,  1),  -- Héctor
-    ( 2, 'INF102', 'Programació',                     297, 1,  2),  -- Marta
-    ( 3, 'INF103', 'Llenguatges de marques',           99, 1,  3),  -- Jordi
-    ( 4, 'INF104', 'Entorns de desenvolupament',       66, 1,  4),  -- Eva (inactiva)
-    ( 5, 'INF105', 'Sistemes informàtics',            165, 1,  2),  -- Marta
-    ( 6, 'INF201', 'Accés a dades',                   132, 2,  1),  -- Héctor
-    ( 7, 'INF202', 'Desenvolupament d''interfícies',  132, 2,  3),  -- Jordi
-    -- Disseny
-    ( 8, 'DIS101', 'Tipografia',                       99, 1,  5),  -- Dori
-    ( 9, 'DIS102', 'Dibuix artístic',                 132, 1,  6),  -- Anna
-    (10, 'DIS201', 'Disseny gràfic editorial',        165, 2,  5),  -- Dori
-    -- Anglès (transversal)
-    (11, 'ENG101', 'Anglès tècnic I',                  99, 1,  7),  -- Ramón
-    (12, 'ENG201', 'Anglès tècnic II',                 99, 2,  8),  -- Sergi
-    -- Orientació Laboral / FOL
-    (13, 'FOL101', 'Formació i orientació laboral',    99, 1,  9),  -- Isabel
-    (14, 'FOL201', 'Empresa i iniciativa emprenedora', 66, 2,  9),  -- Isabel
-    -- Sanitat
-    (15, 'SAN101', 'Anatomia patològica',             132, 1, 10),  -- Pere
-    (16, 'SAN201', 'Biologia molecular',              165, 2, 10),  -- Pere
-    -- Electrònica
-    (17, 'ELE101', 'Electrònica analògica',           165, 1, 13),  -- Carles
-    (18, 'ELE201', 'Electrònica digital',             132, 2, 13),  -- Carles
-    -- Administració
-    (19, 'ADM101', 'Comptabilitat',                   132, 1, 12),  -- Núria
-    (20, 'ADM201', 'Gestió logística',                 99, 2, NULL); -- sense professor
+    ( 1, 'INF101', 'Bases de dades', 165, 1,  1), 
+    ( 2, 'INF102', 'Programació', 297, 1,  2),  
+    ( 3, 'INF103', 'Llenguatges de marques', 99, 1,  3), 
+    ( 4, 'INF104', 'Entorns de desenvolupament', 66, 1,  4), 
+    ( 5, 'INF105', 'Sistemes informàtics', 165, 1,  2),  
+    ( 6, 'INF201', 'Accés a dades', 132, 2,  1),  
+    ( 7, 'INF202', 'Desenvolupament d''interfícies', 132, 2,  3),  
+    ( 8, 'DIS101', 'Tipografia', 99, 1,  5),  
+    ( 9, 'DIS102', 'Dibuix artístic', 132, 1,  6),  
+    (10, 'DIS201', 'Disseny gràfic editorial', 165, 2,  5),  
+    (11, 'ENG101', 'Anglès tècnic I', 99, 1,  7), 
+    (12, 'ENG201', 'Anglès tècnic II', 99, 2,  8),  
+    (13, 'FOL101', 'Formació i orientació laboral', 99, 1,  9), 
+    (14, 'FOL201', 'Empresa i iniciativa emprenedora', 66, 2,  9),  
+    (15, 'SAN101', 'Anatomia patològica', 132, 1, 10),  
+    (16, 'SAN201', 'Biologia molecular', 165, 2, 10),  
+    (17, 'ELE101', 'Electrònica analògica', 165, 1, 13),  
+    (18, 'ELE201', 'Electrònica digital', 132, 2, 13),  
+    (19, 'ADM101', 'Comptabilitat', 132, 1, 12),  
+    (20, 'ADM201', 'Gestió logística', 99, 2, NULL); 
 
 -- ------------------------------------------------------------
 --  MATRÍCULES
