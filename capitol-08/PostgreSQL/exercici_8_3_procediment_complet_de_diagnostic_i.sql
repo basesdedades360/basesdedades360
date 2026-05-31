fadca77485b3ecac954a8da6ddf2b544d24bea82
basesdedades360 <jordi.quesada@proton.me>
2026-05-31 07:28:04 +0200

    Update exercici_8_3_procediment_complet_de_diagnostic_i.sql

--- capitol-08/PostgreSQL/exercici_8_3_procediment_complet_de_diagnostic_i.sql
+++ capitol-08/PostgreSQL/exercici_8_3_procediment_complet_de_diagnostic_i.sql
@@ -1,22 +1,7 @@
 -- Exercici resolt 8.3
--- Procediment complet de diagnòstic i monitoratge
--- Sintaxi: PostgreSQL
+-- PostgreSQL
 
-CREATE INDEX idx_alumne_curs_cover
-  ON alumne(curs_inici)
-  INCLUDE (nom, cognoms);
-
--- --- Bloc següent ---
-
-SELECT a.nom, a.cognoms, AVG(ma.nota_final) AS mitjana
-FROM alumne a
-JOIN matricula ma ON a.id_alumne = ma.id_alumne
-WHERE a.curs_inici = 2024
-GROUP BY a.id_alumne, a.nom, a.cognoms
-ORDER BY mitjana DESC;
-
--- --- Bloc següent ---
-
+-- 1 
 SELECT pid, usename, now() - query_start AS durada,
        state, query
 FROM pg_stat_activity
@@ -25,16 +10,15 @@ WHERE datname = 'institut'
   AND now() - query_start > interval '5 seconds'
 ORDER BY durada DESC;
 
--- --- Bloc següent ---
 
--- Pas 1: intent suau
+-- 2
+-- intent suau
 SELECT pg_cancel_backend(18472);
 
--- Pas 2: si no funciona en 30 segons, intent forçat
+-- si no funciona intent forçat
 SELECT pg_terminate_backend(18472);
 
--- --- Bloc següent ---
-
+-- 3
 SELECT query, calls, total_exec_time, mean_exec_time
 FROM pg_stat_statements
 WHERE query ILIKE '%matricula%'
@@ -42,8 +26,8 @@ WHERE query ILIKE '%matricula%'
 ORDER BY total_exec_time DESC
 LIMIT 5;
 
--- --- Bloc següent ---
 
+-- 4
 EXPLAIN ANALYZE
 SELECT a.nom, a.cognoms, AVG(ma.nota_final) AS mitjana
 FROM alumne a
@@ -51,17 +35,18 @@ JOIN matricula ma ON a.id_alumne = ma.id_alumne
 WHERE a.curs_inici = 2024
 GROUP BY a.id_alumne, a.nom, a.cognoms
 ORDER BY mitjana DESC;
--- Execution Time: 3450 ms
 
--- --- Bloc següent ---
-
+-- 5
 ANALYZE alumne;
 ANALYZE matricula;
-
--- --- Bloc següent ---
 
 CREATE INDEX idx_alumne_curs_inici ON alumne(curs_inici);
 ANALYZE alumne;
 
 EXPLAIN ANALYZE SELECT ...;
 -- Execution Time: 280 ms
+
+CREATE INDEX idx_alumne_curs_cover
+  ON alumne(curs_inici)
+  INCLUDE (nom, cognoms);
+
