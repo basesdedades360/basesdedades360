f953d1af8286c19bdb046c748e79a0eb3cb992ac
basesdedades360 <jordi.quesada@proton.me>
2026-05-31 07:21:44 +0200

    Update exercici_8_1_disseny_d_indexs_i_mesura_del_seu_e.sql

--- capitol-08/PostgreSQL/exercici_8_1_disseny_d_indexs_i_mesura_del_seu_e.sql
+++ capitol-08/PostgreSQL/exercici_8_1_disseny_d_indexs_i_mesura_del_seu_e.sql
@@ -1,7 +1,8 @@
 -- Exercici resolt 8.1
--- Disseny d'índexs i mesura del seu efecte
--- Sintaxi: PostgreSQL
+-- PostgreSQL
 
+
+-- 2
 EXPLAIN ANALYZE
 SELECT a.nom, a.cognoms, ma.nota_final
 FROM alumne a
@@ -9,40 +10,24 @@ JOIN matricula ma ON a.id_alumne = ma.id_alumne
 JOIN modul m      ON ma.id_modul  = m.id_modul
 WHERE m.codi = 'DAW01' AND ma.nota_final < 5;
 
--- --- Bloc següent ---
+-- 3
 
 -- Opció 1: índex per a la FK matricula.id_modul
 CREATE INDEX idx_matricula_modul ON matricula(id_modul);
-
--- --- Bloc següent ---
 
 -- Opció 2: índex parcial sobre suspesos (només PostgreSQL)
 CREATE INDEX idx_matricula_suspesos
   ON matricula(id_modul)
   WHERE nota_final < 5;
 
--- --- Bloc següent ---
+-- 4
 
--- Pas 1: anotar el baseline
 \timing on
 EXPLAIN ANALYZE SELECT ...;
--- Execution Time: 4200 ms
 
--- Pas 2: crear l'índex
 CREATE INDEX idx_matricula_suspesos
   ON matricula(id_modul) WHERE nota_final < 5;
 
--- Pas 3: actualitzar estadístiques perquè el motor el vegi
-ANALYZE matricula;
-
--- Pas 4: tornar a mesurar
 EXPLAIN ANALYZE SELECT ...;
--- Execution Time: 12 ms
 
--- --- Bloc següent ---
 
-SELECT a.nom, a.cognoms, ma.nota_final
-FROM alumne a
-JOIN matricula ma ON a.id_alumne = ma.id_alumne
-JOIN modul m      ON ma.id_modul  = m.id_modul
-WHERE m.codi = 'DAW01' AND ma.nota_final < 5;
